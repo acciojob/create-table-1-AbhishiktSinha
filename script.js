f1be004d9c69e601307bfb440a79a1cc4fe253bf
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-12 10:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const table = document.querySelector("table");
+const tbody = table.querySelector("tbody");
+
 function insert_Row() {
     //Write your code here
+	const firstRow = table.querySelector("tr");
+	const newRow = createNewRow();
+
+	tbody.insertBefore(newRow, firstRow);
   
-  
+}
+
+function createNewRow() {
+	const tr = document.createElement("tr");
+	tr.innerHTML = '<td>New Cell 1</td><td>New Cell 2</td>';
+	
+
+	return tr;
 }
